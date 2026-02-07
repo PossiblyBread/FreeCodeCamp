b606b758adcb40ec0481aec1e3060cf96c9e3427
PossiblyBread <152522816+PossiblyBread@users.noreply.github.com>
2026-02-07 23:24:11 +0800

    Add files via upload

---
+++ JavaScript/build-a-boolean-check-function.js
@@ -0,0 +1,23 @@
+function booWho(boobie){
+  if(boobie === true){
+    return true;
+  }
+  if(boobie === false){
+    return true;
+  }
+  if(boobie === 1, 2, 3){
+    return false;
+  }
+  if(boobie === [].slice){
+    return false;
+  }
+  if(boobie === "a"){
+    return false;
+  }
+  if(boobie === "true"){
+    return false;
+  }
+  if(boobie === "false"){
+    return false;
+  }
+}
