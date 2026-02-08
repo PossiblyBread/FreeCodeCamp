20ecea2b4d32a05132d0682f317a77888f4b3474
PossiblyBread <adrian2zero@gmail.com>
2026-02-08 23:45:57 +0800

    new file:   JavaScript/build-a-leap-year-calculator.js

---
+++ JavaScript/build-a-leap-year-calculator.js
@@ -0,0 +1,23 @@
+function isLeapYear(year){
+  if ((year % 100 == 0) && !(year % 400 == 0)){
+    return year + " is not a leap year.";
+  }
+  else if (year % 4 == 0){
+    return year + " is a leap year.";
+  }
+  else {
+    return year + " is not a leap year.";
+  }
+}
+
+let year = 2024
+let result = isLeapYear(year)
+console.log(result) 
+
+year = 1900
+result = isLeapYear(year)
+console.log(result)
+
+year = 2023
+result = isLeapYear(year)
+console.log(result)
